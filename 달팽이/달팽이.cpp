acf6cc4fd29614d5a08e993d311bd70e33b3756f
DahamChoi <rorigu32@gmail.com>
2021-04-07 22:30:48 +0900

    달팽이

---
+++ 달팽이/달팽이.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> ar(n * n);
+    int x = n / 2, y = n / 2;
+    int dx = 1, dy = 1;
+    int num = 1;
+
+    while (true)
+    {
+        for (int i = y; y > i - dx; --y)
+        {
+            ar[y * n + x] = num++;
+        }
+        ++dy;
+
+        if (num >= n * n)
+            break;
+
+        for (int i = x; x < i + dx; x++)
+        {
+            ar[y * n + x] = num++;
+        }
+        ++dx;
+
+        for (int i = y; y < i + dx; y++)
+        {
+            ar[y * n + x] = num++;
+        }
+        ++dy;
+
+        for (int i = x; x > i - dx; x--)
+        {
+            ar[y * n + x] = num++;
+        }
+        ++dx;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << ar[i * n + j] << " ";
+        }
+        cout << endl;
+    }
+}
