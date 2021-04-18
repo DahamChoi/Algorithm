d2f3f9715e24408f260e251bf22bf99216ad44d8
DahamChoi <rorigu32@gmail.com>
2021-04-18 16:53:13 +0900

    플로이드

---
+++ N번째큰수/N번째큰수.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    int n, num;
+    cin >> n;
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf_s("%d", &num);
+            pq.push(num);
+            if (i >= 1) {
+                pq.pop();
+            }
+        }
+    }
+
+    cout << pq.top();
+}
---
+++ 플로이드/플로이드.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+void floyd(vector<vector<int>>& arr) {
+    int n = arr.size() - 1;
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                arr[i][j] = min(arr[i][k] + arr[k][j], arr[i][j]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, m;
+    cin >> n;
+    cin >> m;
+
+    vector<vector<int>> arr(n + 1, vector<int>(n + 1, 10000000));
+    for (int i = 0; i <= n; i++) {
+        arr[i][i] = 0;
+    }
+
+    for (int i = 0; i < m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        arr[a][b] = c;
+        arr[b][a] = c;
+    }
+
+    floyd(arr);
+
+    int min_index = 1;
+    int min_time = 100000000;
+    for (int i = 1; i <= n; i++) {
+        int sum = 0;
+        for (int j = 1; j <= n; j++) {
+            sum += arr[i][j];
+        }
+        
+        if (sum < min_time) {
+            min_index = i;
+            min_time = sum;
+        }
+    }
+
+    cout << min_index;
+}
