43ea53c0eec4bc2527b6952df676fb97a89eb672
DahamChoi <rorigu32@gmail.com>
2021-05-10 22:15:58 +0900

    문자열 폭발

---
+++ 문자열 폭발/문자열 폭발.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    string original, bomb;
+    cin >> original;
+    cin >> bomb;
+
+    vector<string> stack(1, "");
+    for (int i = 0; i < original.length(); i++) {
+        if (original[i] == bomb.front()) {
+            stack.push_back(string(1, bomb.front()));
+            if (stack.back() == bomb) {
+                stack.pop_back();
+            }
+        }
+        else {
+            stack.back().push_back(original[i]);
+            if (stack.back() == bomb) {
+                stack.pop_back();
+            }
+        }
+    }
+
+    if (stack.size() == 1 && stack.back() == "") {
+        cout << "FRULA";
+    }
+    else {
+        for (auto& it : stack) {
+            cout << it;
+        }
+    }
+
+    return 0;
+}
